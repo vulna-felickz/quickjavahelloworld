8a2d55d03f9adca521ca249a9043bb46bfb43bf5
Daniel Figucio <745341+affrae@users.noreply.github.com>
2021-03-18 20:41:47 +1100

    Create app.java

---
+++ app.java
@@ -0,0 +1,9 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+        String disp = "";
+        for(int i=0; i<31 ; i++)
+            disp = disp + Integer.toString(i);
+        System.out.println(disp); 
+        }
+}
