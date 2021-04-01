04e8d9c1b963fced21ce4328bad95a254f0a012a
Daniel Figucio <745341+affrae@users.noreply.github.com>
2021-04-01 11:44:38 +1100

    Update clibazel.sh

--- clibazel.sh
+++ clibazel.sh
@@ -1,57 +1,47 @@
 #! /bin/zsh
 
-# helloworld.java
+if [[ "$1" == "macos" ]] || [[ "$1" == "linux" ]]
+then
 
-timestamp() {
-  date +"%Y%m%d.%H%M%S" # current time
-}
+  timestamp () {
+    date +"%Y%m%d.%H%M%S" # current time
+  }
 
-THEPATH=$PWD
+  THEPATH=$PWD
 
-BINARY=$THEPATH/codeqlbinaries/$1/codeql
+  BINARY=$THEPATH/codeqlbinaries/$1/codeql
 
-if test -f "$BINARY"; then
-  echo "Executing with codeql cli binary: $BINARY"
+  if test -f "$BINARY"; then
+    echo "Executing with codeql cli binary: $BINARY"
 
-  # rm -rf /private/var/tmp/_bazel_$USERNAME 
+    echo "Brute force removing prior bazel output..."
+    rm -rf bazel-*
 
-  rm -rf bazel-*
+    export SEMMLE_JAVA_INTERCEPT_VERBOSITY=6
+    export SEMMLE_JAVA_INTERCEPT_LOG_FILE=true
 
-  export SEMMLE_JAVA_INTERCEPT_VERBOSITY=6
-  export SEMMLE_JAVA_INTERCEPT_LOG_FILE=true
+    echo "Making sure directory java-database exists (folder and contents not being tracked by git - see .gitignore)"
+    mkdir -p java-database
 
-  mkdir -p java-database
+    TIMESTAMP=$(timestamp)
 
-  ## TEMPLATE
+    echo "Creating database at java-database/$TIMESTAMP"
+    $BINARY database create "java-database/$TIMESTAMP" --language=java --command="bazel shutdown" --command="bazel clean --expunge" --command="bazel build --strategy=local --spawn_strategy=local --nouse_action_cache //:app" -vvvv
+    
+    APP=./bazel-bin/app
 
-  # codeql database create --language java --command "bazel shutdown; bazel build --spawn_strategy=local --nouse_action_cache //path/to/build/targets/..." -vvv --source-root <path to clean source checkout> <output database directory to create>
-
-
-  ## DOES NOT WORK
-
-  # codeql database create "java-database/$(timestamp)" --language=java -vvvv --command="bazel shutdown" --command="bazel clean --expunge" --command "bazel --install_base=/var/tmp/_bazel_dfigucio/install/5ccdfa437f9a94a683a2b6383c9f8794 --output_base=/private/var/tmp/_bazel_dfigucio/822eb5194f92fb5bc2349f393bea30ae exec-server && bazel build --nouse_action_cache --modify_execution_info MNEMONIC=+no-cache //:app" --source-root . 
-
-  ## WORKS
-
-  $BINARY database create "java-database/$(timestamp)" --language=java --command="bazel shutdown" --command="bazel clean" --command="bazel build --strategy=local --spawn_strategy=local --nouse_action_cache //:app" -vvvv
-
-  # $BINARY database create "java-database/$(timestamp)" --language=java -vvvv --command="bazel shutdown" --command="bazel clean --expunge" --command "bazel build --strategy=standalone --spawn_strategy=standalone --nouse_action_cache --modify_execution_info MNEMONIC=+no-cache //:app" --source-root . 
-
-  # $BINARY database create "java-database/$(timestamp)" --language=java -vvvv --command="bazel shutdown" --command="bazel clean --expunge" --command "bazel build --spawn_strategy=local --nouse_action_cache --modify_execution_info MNEMONIC=+no-cache //:app" --source-root . 
-
-  # $BINARY database create "java-database/$(timestamp)" --language=java -vvvv --command="bazel shutdown" --command="bazel clean --expunge" --command "bazel build --strategy=standalone --spawn_strategy=standalone --nouse_action_cache --modify_execution_info MNEMONIC=+no-cache //:app" --source-root . 
-  APP=./bazel-bin/app
-
-  if test -f "$APP"; then
-    echo "Executing $APP"
-    ./bazel-bin/app
-    exit 0
+    if test -f "$APP"; then
+      echo "Executing $APP"
+      ./bazel-bin/app
+    else 
+      echo "$APP does not exist"
+      exit 3
+    fi
   else 
-    echo "$APP does not exist"
+    echo "$BINARY does not exist"
     exit 2
   fi
-else 
-  echo "$BINARY does not exist"
+else
+  echo "Usage $0 macos|linux"
   exit 1
 fi
-
