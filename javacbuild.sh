167ebd9e99f2ced60b1faeeca02599127ae0860b
Daniel Figucio <745341+affrae@users.noreply.github.com>
2021-04-01 11:44:56 +1100

    Update javacbuild.sh

--- javacbuild.sh
+++ javacbuild.sh
@@ -1,25 +1,31 @@
 #! /bin/zsh
 
-GIT_BRANCH="refs/heads/$(git branch --show-current)"
-GIT_HASH=$(git rev-parse HEAD)
+if [[ "$1" == "macos" ]] || [[ "$1" == "linux" ]]
+then
+    GIT_BRANCH="refs/heads/$(git branch --show-current)"
+    GIT_HASH=$(git rev-parse HEAD)
 
-# helloworld.java
-codeql-runner-macos init \
-    --github-url 'https://github.com' \
-    --repository 'affrae/quickhelloworld' \
-    --languages 'java' \
-    --config-file '.github/codeql/codeql-config.yml'
+    # helloworld.java
+    codeql-runner-$1 init \
+        --github-url 'https://github.com' \
+        --repository 'affrae/quickhelloworld' \
+        --languages 'java' \
+        --config-file '.github/codeql/codeql-config.yml'
 
-. codeql-runner/codeql-env.sh
+    . codeql-runner/codeql-env.sh
 
-rm src/main/java/hello/HelloWorld.class
+    rm src/main/java/hello/HelloWorld.class
 
-# Execution from macOS
-$CODEQL_RUNNER javac src/main/java/hello/HelloWorld.java
+    # Execution from macOS
+    $CODEQL_RUNNER javac src/main/java/hello/HelloWorld.java
 
-codeql-runner-macos analyze \
-    --github-url 'https://github.com' \
-    --repository 'affrae/quickhelloworld' \
-    --commit $GIT_HASH \
-    --no-upload \
-    --ref $GIT_BRANCH
+    codeql-runner-$1 analyze \
+        --github-url 'https://github.com' \
+        --repository 'affrae/quickhelloworld' \
+        --commit $GIT_HASH \
+        --no-upload \
+        --ref $GIT_BRANCH
+else
+  echo "Usage $0 macos|linux"
+  exit 1
+fi
