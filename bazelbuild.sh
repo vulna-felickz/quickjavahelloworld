c05d65c94e12669f1da9a6266f67cbbcbc1e388c
Daniel Figucio <745341+affrae@users.noreply.github.com>
2021-04-01 13:23:22 +1100

    removed need for bazelbuild.sh os declaration

--- bazelbuild.sh
+++ bazelbuild.sh
@@ -1,67 +1,74 @@
 #! /bin/zsh
 
-if [[ "$1" == "macos" ]] || [[ "$1" == "linux" ]]
-then
+case "$(uname -s)" in
+    *Linux*)
+        THEOS=linux
+        ;;
+    *Darwin*)
+        THEOS=macos
+        ;;
+    *MINGW* | MSYS*)
+        error "Windows currently not supported."
+        ;;
+    *)
+        error "Unknown operating system '$(uname -s)' (full uname: $(uname -a)."
+esac
 
-  GIT_BRANCH="refs/heads/$(git branch --show-current)"
-  GIT_HASH=$(git rev-parse HEAD)
+GIT_BRANCH="refs/heads/$(git branch --show-current)"
+GIT_HASH=$(git rev-parse HEAD)
 
-  THEPATH=$PWD
-  BUNDLE=$THEPATH/codeqlbinaries/$1
-  THECONFIGFILE=$THEPATH/.github/codeql/codeql-config.yml
+THEPATH=$PWD
+BUNDLE=$THEPATH/codeqlbinaries/$THEOS
+THECONFIGFILE=$THEPATH/.github/codeql/codeql-config.yml
 
-  if test -d "$BUNDLE"; then
-    echo "Executing with:"
-    echo "... the path to where we executed this from: $THEPATH"
-    echo "... codeql cli binary: $BUNDLE/codeql"
-    echo "... config file: $THECONFIGFILE"
+if test -d "$BUNDLE"; then
+  echo "Executing with:"
+  echo "... the path to where we executed this from: $THEPATH"
+  echo "... codeql cli binary: $BUNDLE/codeql"
+  echo "... config file: $THECONFIGFILE"
 
-    rm -rf $THEPATH/bazel-*
+  rm -rf $THEPATH/bazel-*
 
-    export SEMMLE_JAVA_INTERCEPT_VERBOSITY=6
-    export SEMMLE_JAVA_INTERCEPT_LOG_FILE=true
-    
-    codeql-runner-$1 init \
-      --github-url 'https://github.com' \
-      --repository 'affrae/quickhelloworld' \
-      --languages 'java' \
-      --codeql-path $BUNDLE/codeql \
-      --config-file $THECONFIGFILE \
-      --debug
-    
-    . $THEPATH/codeql-runner/codeql-env.sh
+  export SEMMLE_JAVA_INTERCEPT_VERBOSITY=6
+  export SEMMLE_JAVA_INTERCEPT_LOG_FILE=true
+  
+  codeql-runner-$THEOS init \
+    --github-url 'https://github.com' \
+    --repository 'affrae/quickhelloworld' \
+    --languages 'java' \
+    --codeql-path $BUNDLE/codeql \
+    --config-file $THECONFIGFILE \
+    --debug
+  
+  . $THEPATH/codeql-runner/codeql-env.sh
 
+  echo "\$CODEQL_RUNNER = $CODEQL_RUNNER" 
+
+  bazel shutdown 
+
+  bazel clean --expunge
+
+  if [[ "$THEOS" == "macos" ]]
+  then
+  # Execution from macOS
     echo "\$CODEQL_RUNNER = $CODEQL_RUNNER" 
+    $CODEQL_RUNNER bazel build --spawn_strategy=local --nouse_action_cache //:app
+  else
+    bazel build --spawn_strategy=local --nouse_action_cache //:app
+  fi
 
-    bazel shutdown 
+  codeql-runner-$THEOS analyze --github-url 'https://github.com' --repository 'affrae/quickhelloworld' --commit $GIT_HASH --no-upload --ref $GIT_BRANCH
 
-    bazel clean --expunge
+  APP=$THEPATH/bazel-bin/app
 
-    if [[ "$1" == "macos" ]]
-    then
-    # Execution from macOS
-      echo "\$CODEQL_RUNNER = $CODEQL_RUNNER" 
-      $CODEQL_RUNNER bazel build --spawn_strategy=local --nouse_action_cache //:app
-    else
-      bazel build --spawn_strategy=local --nouse_action_cache //:app
-    fi
-
-    codeql-runner-$1 analyze --github-url 'https://github.com' --repository 'affrae/quickhelloworld' --commit $GIT_HASH --no-upload --ref $GIT_BRANCH
-
-    APP=$THEPATH/bazel-bin/app
-
-    if test -f "$APP"; then
-      echo "Executing $APP"
-      $THEPATH/bazel-bin/app
-    else 
-      echo "$APP does not exist"
-      exit 3
-    fi
+  if test -f "$APP"; then
+    echo "Executing $APP"
+    $THEPATH/bazel-bin/app
   else 
-    echo "$BUNDLE does not exist"
+    echo "$APP does not exist"
     exit 2
   fi
-else
-  echo "Usage $0 macos|linux"
+else 
+  echo "$BUNDLE does not exist"
   exit 1
 fi
